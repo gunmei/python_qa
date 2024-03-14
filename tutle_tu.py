ee2a71f84cd9b71590aa7dac2a6a4ca1eed52eef
Andrey <andrei676564@gmail.com>
2024-03-14 13:53:11 +0300

    Рисование квадрата

---
+++ tutle_tu.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color("red", "green")
+turtle.speed(5)
+turtle.pensize(5)
+
+for i in range(4):
+    turtle.forward(150)
+    turtle.left(90)
+
+turtle.done()
